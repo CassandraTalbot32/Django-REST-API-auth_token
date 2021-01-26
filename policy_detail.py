f1a2003e0cb714359995ee076ab02332d4368b6a
Cassandra Talbot <40823509+CassandraTalbot32@users.noreply.github.com>
2021-01-26 20:32:41 +0000

    Create policy_detail.py

---
+++ policy_detail.py
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<head>
+<style>
+h1,
+{	color: #F52F57;
+	font-size: 50px;
+	font-family:'impact';
+	float: left;
+	width: 90%;
+	height: 190%;
+	padding: 30px;
+}
+p,
+{	color: #000000;
+	font-size: 25px;
+	font-family:'impact';
+	float: left;
+	
+}
+
+</style>
+
+<h1>My Policy</h1>
+<span>
+
+
+
+</span>
+{% block content %}
+<p>Policy reference:
+apple orange pear</p>
+
+<p>Cover Type:
+comprehensive</p>
+
+<p>Car
+Tesla S Black</p>
+
+<p>Address
+Flat 1, 11 The Street, Little Hampton W53RT</p>
+{% endblock %}
